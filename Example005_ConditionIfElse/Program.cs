4b99c4d26075a2b317c3c74e266f835ea76d1318
Onixfff <danilnikonov01@mail.ru>
2023-06-22 12:02:07 +0200

    добавил новые задачи

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine()!;
+if(userName.ToLower() == "маша")
+    Console.WriteLine("Ура это же Маша!");
+else
+    Console.WriteLine("Привет, " + userName);
---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 20,
+    xc = 80, yc = 20;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+
+int count = 0;
+int max = 3;
+int min = 0;
+Random random = new Random();
+while(count < 1000)
+{
+    int what = random.Next(min, max);
+    switch (what)
+    {
+        case 0:
+            x = (x + xa) / 2;
+            y = (y + ya) / 2;
+        break;
+        case 1:
+            x = (x + xb) / 2;
+            y = (y + yb) / 2;
+            break;
+        case 2:
+            x = (x + xc) / 2;
+            y = (y + yc) / 2;
+            break;
+    }
+
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine("+");
+    count ++;
+}
