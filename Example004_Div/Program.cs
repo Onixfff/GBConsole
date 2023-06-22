15c96e5f74a50fd0ce5c900d3019b59908711fbb
Onixfff <danilnikonov01@mail.ru>
2023-06-22 10:44:37 +0200

    изменил 4 папку и внес туда random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,6 @@
-﻿float numberFirst = 12;
-float numberSecond = 5;
-Console.WriteLine(numberFirst/numberSecond);
+﻿int min = 1;
+int max = 10;
+Random random = new Random();
+float numberFirst = random.Next(min, max);
+float numberSecond = random.Next(min, max);
+Console.WriteLine($"Первое число - {numberFirst}\nВторое число - {numberSecond}\nSun = {numberFirst/numberSecond}");
