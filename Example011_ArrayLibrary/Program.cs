f7c6cfca6855044062e6c65b3ee3ffe928497480
Onixfff <danilnikonov01@mail.ru>
2023-06-24 15:47:10 +0200

    Добавил файлы из 2 лекции

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillArray(int[] collection)
+{
+    int max = 10;
+    int min = 1;
+    Random random = new Random();
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = random.Next(min, max);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while(position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+int pos = IndexOff(array, 4);
+Console.WriteLine(pos);
